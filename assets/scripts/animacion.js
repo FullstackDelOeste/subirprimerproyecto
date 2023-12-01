6b306eb3f37a9f318396cffcdc6284ca02246bff
FullstackDelOeste <142056061+FullstackDelOeste@users.noreply.github.com>
2023-12-01 19:13:09 -0300

    Add files via upload
    nuevos1

---
+++ assets/scripts/animacion.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector(".contenedor_evento").classList.add("show");
+    }, 3000);
+});
+
+
+        
